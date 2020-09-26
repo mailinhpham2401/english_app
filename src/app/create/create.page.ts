bfcc34c3647b01bbacf473266233d431e7389fa2
mailinh2401 <linh@wohlstifter.de>
2020-09-26 17:23:14 +0200

    finishhh

--- src/app/create/create.page.ts
+++ src/app/create/create.page.ts
@@ -17,46 +17,6 @@ import { AngularFirestore } from "@angular/fire/firestore";
   styleUrls: ["./create.page.scss"],
 })
 export class CreatePage implements OnInit {
-  /*  flipped: boolean = true;
-  public addFlashCard: FormGroup;
-
-  constructor(
-    public loadingCtrl: LoadingController,
-    public alertCtrl: AlertController,
-    public firebaseService: FirebaseService,
-    private router: Router,
-    formBuilder: FormBuilder
-  ) {
-    this.addFlashCard = formBuilder.group({
-      front: ["", Validators.required],
-      back: ["", Validators.required],
-    });
-  }
-
-  ngOnInit() {}
-  flip() {
-    this.flipped = !this.flipped;
-  }
-  async createFlashCard() {
-    const loading = await this.loadingCtrl.create();
-
-    const front = this.addFlashCard.value.front;
-    const back = this.addFlashCard.value.back;
-
-    this.firebaseService.getFlashCards().subscribe(
-      () => {
-        loading.dismiss().then(() => {
-          this.router.navigateByUrl("");
-        });
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-
-    return await loading.present();
-	} */
-
   flashcard = {} as FlashCard;
 
   constructor(

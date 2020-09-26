699671f85aa17be0ff4b9e50ef07a6050ac8e575
mailinh2401 <linh@wohlstifter.de>
2020-09-26 12:45:33 +0200

    finish get,search,add flashcards function

--- src/app/create/create.page.ts
+++ src/app/create/create.page.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { LoadingController, AlertController } from "@ionic/angular";
+import {
+  LoadingController,
+  AlertController,
+  ToastController,
+  NavController,
+} from "@ionic/angular";
 import { FirebaseService } from "../services/data/firebase.service";
 import { Router } from "@angular/router";
+import { FlashCard } from "../models/flash-card.interface";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
   selector: "app-create",
@@ -10,7 +17,7 @@ import { Router } from "@angular/router";
   styleUrls: ["./create.page.scss"],
 })
 export class CreatePage implements OnInit {
-  flipped: boolean = true;
+  /*  flipped: boolean = true;
   public addFlashCard: FormGroup;
 
   constructor(
@@ -48,5 +55,28 @@ export class CreatePage implements OnInit {
     );
 
     return await loading.present();
+	} */
+
+  flashcard = {} as FlashCard;
+
+  constructor(
+    private toastCtrl: ToastController,
+    private navCtrl: NavController,
+    private firestore: AngularFirestore
+  ) {}
+  ngOnInit() {}
+
+  async createFlashCard(flashcard: FlashCard) {
+    this.firestore.collection("flashcards").add(flashcard);
+    this.navCtrl.navigateRoot("home");
+  }
+
+  showToast(message: string) {
+    this.toastCtrl
+      .create({
+        message: message,
+        duration: 3000,
+      })
+      .then((toastData) => toastData.present());
   }
 }
